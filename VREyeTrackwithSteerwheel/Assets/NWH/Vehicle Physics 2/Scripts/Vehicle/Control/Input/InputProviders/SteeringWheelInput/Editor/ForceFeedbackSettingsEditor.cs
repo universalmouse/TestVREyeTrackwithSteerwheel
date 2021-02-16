b7b366ba12a076d64c9ce3246d651dbfba991ff6
universalmouse <75366209+universalmouse@users.noreply.github.com>
2021-02-16 18:52:17 +0800

    Create a project

---
+++ VREyeTrackwithSteerwheel/Assets/NWH/Vehicle Physics 2/Scripts/Vehicle/Control/Input/InputProviders/SteeringWheelInput/Editor/ForceFeedbackSettingsEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using NWH.NUI;
+using UnityEditor;
+using UnityEngine;
+
+namespace NWH.VehiclePhysics2.Input
+{
+    [CustomEditor(typeof(ForceFeedbackSettings))]
+    public class ForceFeedbackSettingsEditor : NUIEditor
+    {
+        public override bool OnInspectorNUI()
+        {
+            if (!base.OnInspectorNUI())
+            {
+                return false;
+            }
+
+            drawer.Info("Vehicle-specific FFB settings.");
+            drawer.Field("overallCoeff");
+            drawer.Field("frictionCoeff");
+            drawer.Field("lowSpeedFrictionCoeff");
+            drawer.Field("satCoeff", true, null, "Self Aligning Torque Coeff");
+            drawer.Field("centeringCoeff");
+
+            drawer.EndEditor(this);
+            return true;
+        }
+
+        public override bool UseDefaultMargins()
+        {
+            return false;
+        }
+    }
+}
